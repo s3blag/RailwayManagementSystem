5499999506309941674be372465d120e6ea55d79
s3blag <226173@student.pwr.edu.pl>
2017-12-10 21:38:36 +0100

    Dodany szablon projektu.

---
+++ RailwayManagementSystem/RailwayManagementSystem/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RailwayManagementSystem
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection sqlConnection;
+        SqlDataAdapter sqlDataAdapter;
+
+        public Form1()
+        {
+            InitializeComponent();
+            sqlConnection = new SqlConnection("Data Source=DESKTOP-G92BDEO\\SQLEXPRESS; database=SRBK_database;Trusted_Connection=yes");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sqlDataAdapter = new SqlDataAdapter("SELECT * FROM SHOW_TRAINS", sqlConnection);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+        }
+    }
+}
---
+++ RailwayManagementSystem/RailwayManagementSystem/Visits.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RailwayManagementSystem
+{
+    class Visits
+    {
+    }
+}
