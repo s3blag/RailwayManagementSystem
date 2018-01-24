83bf268d7ffca11de8cb75093ca3a28c75b58be2
s3blag <226173@student.pwr.edu.pl>
2018-01-24 01:23:05 +0100

    Dodane Logins

---
+++ RailwayManagementSystem/RailwayManagementSystem/Controllers/Logins.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RailwayManagementSystem
+{
+    public static class Logins
+    {
+        public static bool? Validate(string login, string password, SqlConnection sqlConnection)
+        {
+            try
+            {
+                var cmd = new SqlCommand("SELECT COUNT (*) FROM LOGINS WHERE LOGIN=@usr AND PASSWORD=@pwd", sqlConnection);
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@usr", login);
+                cmd.Parameters.AddWithValue("@pwd", password);
+                sqlConnection.Open();
+                if (cmd.ExecuteScalar().ToString() == "1")
+                    return true;
+                else
+                    return false;
+            }
+            catch
+            {
+                return null;
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+    }
+}
